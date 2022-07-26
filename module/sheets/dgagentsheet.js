6387c123e6cee8ecfbaa928f67e7107313510df0
wush3 <wush_3@hotmail.com>
2022-07-26 03:30:16 +0200

    aut skill improvement and context menu working

--- module/sheets/dgagentsheet.js
+++ module/sheets/dgagentsheet.js
@@ -11,10 +11,67 @@ export default class DgAgentSheet extends ActorSheet {
     getData() {
         const data = super.getData();
         data.config = CONFIG.dgalt;
+
         data.skills = data.items.filter(function (item) { return item.type == "Skill" });
+
+        data.skills.sort(function (a, b) {
+            let nameA = a.name.toUpperCase();
+            let nameB = b.name.toUpperCase();
+            if (nameA < nameB) {
+                return -1;
+            }
+            if (nameA > nameB) {
+                return 1;
+            }
+            return 0;
+        });
+
         data.bonds = data.items.filter(function (item) { return item.type == "Bond" });
+        data.bonds.sort();
+
+        data.bonds.sort(function (a, b) {
+            let nameA = a.name.toUpperCase();
+            let nameB = b.name.toUpperCase();
+            if (nameA < nameB) {
+                return -1;
+            }
+            if (nameA > nameB) {
+                return 1;
+            }
+            return 0;
+        });
+
         return data;
     }
+
+    skillContextMenu = [
+        {
+            name: game.i18n.localize("dgalt.labels.contextmenu.edit"),
+            icon: '<i class="fas fa-edit"/>',
+            callback: element => {
+                console.log(this.actor.name);
+                const item = this.actor.items.get(element.data("item-id"));
+                item.sheet.render(true);
+            }
+
+
+        },
+        {
+            name: game.i18n.localize("dgalt.labels.contextmenu.delete"),
+            icon: '<i class="fas fa-trash"/>',
+            callback: element => {
+                console.log(this.actor.name);
+                const item = this.actor.items.get(element.data("item-id"));
+                if (!item) return;
+                return item.delete();
+            }
+        }
+
+
+    ]
+
+
+
 
     activateListeners(html) {
 
@@ -23,42 +80,43 @@ export default class DgAgentSheet extends ActorSheet {
         html.find(".item-delete").click(this._onItemDelete.bind(this));
         html.find(".item-edit").click(this._onItemEdit.bind(this));
         html.find(".item-checkmark").click(this._onCheckboxClick.bind(this));
+        html.find(".skill-improvement").click(this._onImproveClick.bind(this));
 
         html.find(".inline-edit").change(this._onInlineChanged.bind(this));
-        
 
-        
+
+        new ContextMenu(html, ".skill-card", this.skillContextMenu);
+
+
+
 
         super.activateListeners(html);
     }
 
-    async _onItemCreate(event) { 
+    async _onItemCreate(event) {
         event.preventDefault();
         const header = event.currentTarget;
         const type = header.dataset.type;
-                
-        let itemData={
-            name: game.i18n.localize("dgalt.sheet.newItem"),           
+
+        let itemData = {
+            name: game.i18n.localize("dgalt.sheet.newItem"),
             type: type,
             data: foundry.utils.deepClone(header.dataset)
         }
 
-        console.log(itemData.name);
-        console.log(itemData.type);
-
         delete itemData.data.type;
         return this.actor.createEmbeddedDocuments("Item", [itemData]);
     }
-    
+
     async _onItemDelete(event) {
         event.preventDefault();
         const li = event.currentTarget.closest(".item");
         const item = this.actor.items.get(li.dataset.itemId);
-        if ( !item ) return;
+        if (!item) return;
         return item.delete();
-      }
-    
-    async _onItemEdit(event) { 
+    }
+
+    async _onItemEdit(event) {
         event.preventDefault();
         const li = event.currentTarget.closest(".item");
         const item = this.actor.items.get(li.dataset.itemId);
@@ -66,22 +124,43 @@ export default class DgAgentSheet extends ActorSheet {
     }
 
     async _onCheckboxClick(event) {
-        event.preventDefault();    
-        const element= event.currentTarget;    
+        event.preventDefault();
+        const element = event.currentTarget;
         const itemId = element.closest(".item").dataset.itemId;
         const item = this.actor.items.get(itemId);
-        const field=element.dataset.field;
-        return item.update({[field]: !getProperty(item.data, field)});
+        const field = element.dataset.field;
+        return item.update({ [field]: !getProperty(item.data, field) });
     }
 
 
-    async _onInlineChanged(event){
+    async _onInlineChanged(event) {
         event.preventDefault();
-        const element= event.currentTarget;
+        const element = event.currentTarget;
         const itemId = element.closest(".item").dataset.itemId;
-        const item=this.actor.items.get(itemId);
-        const field=element.dataset.field;
-        return item.update({[field]: element.value});
+        const item = this.actor.items.get(itemId);
+        const field = element.dataset.field;
+        return item.update({ [field]: element.value });
+    }
+
+    async _onImproveClick(event) {
+
+        
+        this.actor.items.forEach(item => {
+           
+            if(item.type=="Skill")            
+                if(item.data.data.failcheck)
+                {
+                    //send a basic chatmessage later
+
+                    item.update({["data.value"]: getProperty(item.data,"data.value") + Math.floor(Math.random() * 4)+1})
+
+                    item.update({["data.failcheck"]:false});
+                   
+                   
+                }
+        });
+
+        return;
     }
 
 }
--- module/sheets/dgitemsheet.js
+++ module/sheets/dgitemsheet.js
@@ -4,8 +4,8 @@ export default class DgItemSheet extends ItemSheet {
     {
         return mergeObject(super.defaultOptions,
             {
-                width:340,
-                height:200,
+                width:360,
+                height:300,
                 classes:["dgalt","sheet","item"]
             });
     }
@@ -21,6 +21,35 @@ export default class DgItemSheet extends ItemSheet {
         const data = super.getData();
         data.config=CONFIG.dgalt;
         return data;
+    }
+
+
+    activateListeners(html) {
+       
+        
+        html.find(".item-checkmark").click(this._onCheckboxClick.bind(this));
+        html.find(".inline-edit").change(this._onInlineChanged.bind(this));
+                
+        super.activateListeners(html);
+    }
+
+   
+
+    async _onCheckboxClick(event) {
+        event.preventDefault();    
+        const element= event.currentTarget;                           
+        const item = this.item;
+        const field=element.dataset.field;
+        return item.update({[field]: !getProperty(item.data, field)});
+    }
+
+
+    async _onInlineChanged(event){
+        event.preventDefault();
+        const element= event.currentTarget;        
+        const item=this.item;
+        const field=element.dataset.field;
+        return item.update({[field]: element.value});
     }
 
 }
